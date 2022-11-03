c273ba4c98973396b099cde777cff6a1ad2ca86f
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-03 12:18:36 -0700

    plants data in place and test running for List Route resource # 1

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,20 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const { plants } = require('../lib/plants-data.js');
+
+describe('plants routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
-  afterAll(() => {
-    pool.end();
+
+  it('/plants should return a list of plant botany', async () => {
+    const res = await request(app).get('/plants');
+    const expected = plants.map((plant) => {
+      return { id: plant.id, biomeName: plant.biome_name };
+    });
+    expect(res.body).toEqual(expected);
   });
 });
